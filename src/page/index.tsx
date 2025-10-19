62233ded08bb3872cf1d4f1cbf382f848fc7c582
CokeWithIce <jklkofjklkof@163.com>
2025-10-19 17:38:34 +0800

    第一次提交模板内容。

---
+++ src/page/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Button } from "antd";
+export function Index(){
+    return (
+        <>
+            <div>新页面</div>
+            <Button type="primary" size="large">新增</Button>
+        </>
+    );
+}
