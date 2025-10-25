0556144199e0597818c3b91a85b30f292d0f95e5
CokeWithIce <jklkofjklkof@163.com>
2025-10-25 21:41:53 +0800

    新增了自动引入补丁的组件，页面进行了修改。

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React from "react";
 import { Button } from "antd";
 export function Index(){
     return (
         <>
             <div>新页面</div>
             <Button type="primary" size="large">新增</Button>
+            <div className="text-blue text-40px bg-red">小滴课堂</div>
         </>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnoCss from 'unocss/vite';
+import AutoImport from "unplugin-auto-import/vite"
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    UnoCss(),
+    AutoImport({
+      dts:"src/types/auto-imports.d.ts",
+      imports:["react"],
+      dirs:["./src/hooks"],
+    })
+  ],
 })
