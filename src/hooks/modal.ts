f4071dd0dc04d7a3894af9b82440a6bea87b3d9a
CokeWithIce <jklkofjklkof@163.com>
2025-10-29 21:28:31 +0800

    登录页面新增一个

--- src/hooks/modal.ts
+++ src/hooks/modal.ts
@@ -2,11 +2,22 @@
 import {create} from "zustand"
 //设定一个state
 interface ModalState{
+    //注册的接口
     regVisible:boolean;
     switchRegVisible:()=>void;
+    //登录的接口
+    loginVisible:boolean;
+    SwitchLoginVisible:()=>void;
+
 }
 //修改regVisible的值。
 export const useModal=create<ModalState>((set)=>({
+    //实现判断注册接口，
     regVisible:false,
-    switchRegVisible:()=>set((state)=>({regVisible:!state.regVisible}),false)
+    //实现注册转换接口
+    switchRegVisible:()=>set((state)=>({regVisible:!state.regVisible}),false),
+    //实现判断登录接口，
+    loginVisible:false,
+    //实现登录页面是否出现状态的接口。
+    SwitchLoginVisible:()=>set((state)=>({loginVisible:!state.loginVisible}),false)
 }));
